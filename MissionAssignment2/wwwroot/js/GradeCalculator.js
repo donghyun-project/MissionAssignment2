2a47cec2fb6ca4bc11682ee244627e945cac8c91
dongkya <kdh2395478@gmail.com>
2022-01-18 22:24:09 -0700

    # Assignment 3

--- MissionAssignment2/wwwroot/js/GradeCalculator.js
+++ MissionAssignment2/wwwroot/js/GradeCalculator.js
@@ -1,49 +1,60 @@
 ﻿// Author: Logan Kim
-// Mission #2 Assignment
+// Mission #3 Assignment
+
 
 // Calculate Final Grade and Show through alert
 $("#Calculate").click(function () {
-    GradeScore = Math.round((($("#txtAssignments").val() * 0.55) +
-        ($("#txtGP").val() * 0.05) +
-        ($("#txtQuizzes").val() * 0.1) +
-        ($("#txtExams").val() * 0.2) +
-        ($("#txtINTEX").val() * 0.1)) * 100) / 100;
 
-    console.log("click");
-
-    
-    if (GradeScore >= 93) {
-        GradeLetter = "A";
-    } else if (GradeScore >= 90) {
-        GradeLetter = "A-";
-    } else if (GradeScore >= 87) {
-        GradeLetter = "B+";
-    } else if (GradeScore >= 83) {
-        GradeLetter = "B";
-    } else if (GradeScore >= 80) {
-        GradeLetter = "B-";
-    } else if (GradeScore >= 77) {
-        GradeLetter = "C+";
-    } else if (GradeScore >= 73) {
-        GradeLetter = "C";
-    } else if (GradeScore >= 70) {
-        GradeLetter = "C-";
-    } else if (GradeScore >= 67) {
-        GradeLetter = "D+";
-    } else if (GradeScore >= 63) {
-        GradeLetter = "D";
-    } else if (GradeScore >= 60) {
-        GradeLetter = "D-";
+    // Checks if the score values are in the range (0, 100)
+    if (($("#txtAssignments").val() > 100) || ($("#txtAssignments").val() < 0) || ($("#txtAssignments").val() == "")) {
+        alert("Cannot Calculate");
+    } else if (($("#txtGP").val() > 100) || ($("#txtGP").val() < 0) || ($("#txtGP").val() == "")) {
+        alert("Cannot Calculate");
+    } else if (($("#txtQuizzes").val() > 100) || ($("#txtQuizzes").val() < 0) || ($("#txtQuizzes").val() == "")) {
+        alert("Cannot Calculate");
+    } else if (($("#txtExams").val() > 100) || ($("#txtExams").val() < 0) || ($("#txtExams").val() == "")) {
+        alert("Cannot Calculate");
+    } else if (($("#txtINTEX").val() > 100) || ($("#txtINTEX").val() < 0) || ($("#txtINTEX").val() == "")) {
+        alert("Cannot Calculate");
     } else {
-        GradeLetter = "E";
+        // Calculates the Final Grade (Number)
+        GradeScore = Math.round((($("#txtAssignments").val() * 0.55) +
+            ($("#txtGP").val() * 0.05) +
+            ($("#txtQuizzes").val() * 0.1) +
+            ($("#txtExams").val() * 0.2) +
+            ($("#txtINTEX").val() * 0.1)) * 100) / 100;
+
+        // Calculates the Letter Grade
+        if (GradeScore >= 93) {
+            GradeLetter = "A";
+        } else if (GradeScore >= 90) {
+            GradeLetter = "A-";
+        } else if (GradeScore >= 87) {
+            GradeLetter = "B+";
+        } else if (GradeScore >= 83) {
+            GradeLetter = "B";
+        } else if (GradeScore >= 80) {
+            GradeLetter = "B-";
+        } else if (GradeScore >= 77) {
+            GradeLetter = "C+";
+        } else if (GradeScore >= 73) {
+            GradeLetter = "C";
+        } else if (GradeScore >= 70) {
+            GradeLetter = "C-";
+        } else if (GradeScore >= 67) {
+            GradeLetter = "D+";
+        } else if (GradeScore >= 63) {
+            GradeLetter = "D";
+        } else if (GradeScore >= 60) {
+            GradeLetter = "D-";
+        } else {
+            GradeLetter = "E";
+        };
+
+        // Alerts Final Grade (Number) and Letter Grade
+        alert("Final Grade: " +
+            GradeScore +
+            "\nLetter Grade: " + GradeLetter);
     } 
 
-    // Alerts Final Grade and Letter Grade
-    alert("Final Grade: " +
-        GradeScore +
-        "\nLetter Grade: " + GradeLetter);
-
-    //$("#GradeScore").innerHTML = GradeScore;
-    //$("#GradeLetter").innerHTML = GradeLetter;
-
-})
+});
